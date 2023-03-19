bf8cc83bf2b8d7e0bee490956b0322d1dc255600
bispo-daniel <dbispo.work@gmail.com>
2023-03-19 01:06:31 -0300

    update

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const perf = () => {
-    const duration = performance.getEntriesByType("navigation")[0].duration;
-    if (!duration) setTimeout(perf, 0);
-    else console.log({ duration });
-}
-window.addEventListener('DOMContentLoaded', perf);
